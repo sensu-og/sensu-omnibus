d74c6f70b77d64dd24796835d2ae4d3aa4965c9d
Justin Kolberg <amd.prophet@gmail.com>
2019-03-05 16:59:14 -0800

    bump ruby to 2.4.5
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- config/projects/sensu.rb
+++ config/projects/sensu.rb
@@ -40,7 +40,7 @@ build_version version
 build_iteration ENV["BUILD_NUMBER"]
 
 override "sensu-gem", version: version
-override "ruby", version: "2.4.4"
+override "ruby", version: "2.4.5"
 override "rubygems", version: "2.7.9"
 
 package :deb do
